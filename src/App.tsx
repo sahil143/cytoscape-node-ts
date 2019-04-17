ada409bead8b0e6928d30b75f41a22b4b389c8a7
sahil143 <sahilbudhwar143@gmail.com>
2019-04-17 17:18:50 +0530

    add D3DonutComp to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SvgComp from './svgComp';
-import {data} from './data';
+import {data, pods} from './data';
+import Cytoscape from './cytoscape';
+import D3Donut from './d3-node/d3DonutComp';
 
-class App extends Component {
+type state = {pods?: any};
+type props = {};
+class App extends Component<props, state> {
+  pods: any;
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      pods: pods
+    };
+    this.pods = pods
+  }
+
+  podUp = () => {
+    let add = {id: this.pods.length + 1, color: `#${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}${Math.floor(Math.random() * 10)}`}
+    this.pods.push(add);
+    console.log('+', this.pods);
+    this.setState({pods: this.pods});
+  }
+
+  podDown = () => {
+    if(this.pods.length > 1) this.pods.pop();
+    console.log("-", this.pods);
+    this.setState({pods: this.pods});
+  }
+
   render() {
     return (
       <div className="App">
-        {data.map((d,) => <div key={d.data.id}><SvgComp {...d}/></div>)}
+        {/* <button onClick={this.podUp}>+</button><button onClick={this.podDown}>-</button> */}
+        {/* <SvgComp pods={this.state.pods} /> */}
+        {/* {data.map((d,) => <div key={d.data.id}><SvgComp {...d}/></div>)} */}
+      {/* <Cytoscape /> */}
+        <D3Donut data={[4,5,6,7,2]} />
       </div>
     );
   }
